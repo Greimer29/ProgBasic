bba610b8eebbd1a6f8fd00f36fcd64e79e9b0cd7
Greimer29 <greimerabelguzman29@gmail.com>
2020-11-11 17:47:53 -0400

    terminamos de mostrar las imagenens con eventos

--- calculadora de peso universal/villa platzi/platzi.js
+++ calculadora de peso universal/villa platzi/platzi.js
@@ -1,11 +1,10 @@
 var vp = document.getElementById("villa-platzi");
 var papel = vp.getContext("2d");
-var mapa = "tile.png";
 
 //creamos los objetos de las imágenes
 
 var fondo = {
-    url: "fondo.png",
+    url: "tile.png",
     cargaOK: false
 };
 
@@ -14,26 +13,33 @@ var vaca = {
     cargaOK: false
 };
 
-var fondo.imagen = new Image();
-fondo.imagen.src = mapa;
+
+fondo.imagen = new Image();
+fondo.imagen.src = fondo.url;
 fondo.imagen.addEventListener("load", cargarFondo);
 
 function cargarFondo() {
     fondo.cargaOK = true;
-}
-
-function dibujar() {
-    papel.drawImage(fondo, 0, 0);
+    dibujar();
 }
 
 vaca.imagen = new Image();
-vaca.imagen.src = "vaca.png";
+vaca.imagen.src = vaca.url;
 vaca.imagen.addEventListener("load", cargarvaca);
 
-function dibujarvaca() {
-    papel.drawImage(vaca, 10, 20);
+function cargarvaca() {
+    vaca.cargaOK = true;
+    dibujar();
 }
 
+function dibujar() {
+    if (fondo.cargaOK == true) {
+        papel.drawImage(fondo.imagen, 0, 0);
+    }
+    if (vaca.cargaOK == true) {
+        papel.drawImage(vaca.imagen, 100, 100);
+    }
+}
 
 //sacar un número aleatorio.
 function aleatorio(min, max) {
@@ -42,3 +48,4 @@ function aleatorio(min, max) {
     resultado = Math.floor(Math.random() * (max - min + 1)) + min;
     return resultado;
 }
+console.log(papel);
